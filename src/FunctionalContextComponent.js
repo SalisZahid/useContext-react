a6e6e31cbdf2c62ad59b0390a82149ebfdb8fa3e
unknown <salis.zahid@optimusfox.com>
2021-10-13 14:06:35 +0500

    files deleted

---
+++ src/FunctionalContextComponent.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { useTheme, useThemeUpdate } from "./ThemeContext";
+
+export default function FunctionalContextComponent() {
+  const darkTheme = useTheme();
+  const toggleTheme = useThemeUpdate();
+
+  const themeStyles = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    aligSelf: "center",
+    backgroundColor: darkTheme ? "black" : "whiteSmoke",
+    color: darkTheme ? "white" : "black",
+    width: "50%",
+    height: "50%",
+    padding: "2rem",
+  };
+
+  return (
+    <>
+      <button onClick={toggleTheme}>Toggle theme</button>
+
+      <div style={themeStyles}>Funtional Context theme</div>
+    </>
+  );
+}
